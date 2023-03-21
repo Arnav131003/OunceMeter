ceeeee7cded5f70c77f1b6c7994fdf476d2dd96f
Arnav131003 <75151775+Arnav131003@users.noreply.github.com>
2023-03-21 10:33:26 +0530

    Removed whitespaces

--- File.py
+++ File.py
@@ -1,156 +1,51 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-# In[2]:
-
-
 df = pd.read_csv('water_potability.csv')
 df.head()
-
-
-# In[3]:
-
-
 df.shape
-
-
-# In[4]:
-
-
 df.isnull().sum()
-
-
-# In[5]:
-
-
 df.info()
-
-
-# In[6]:
-
-
 df.describe()
-
-
-# In[7]:
-
-
 df.fillna(df.mean(), inplace=True)
 df.isnull().sum()
-
-
-# In[8]:
-
-
 df.Potability.value_counts()
-
-
-# In[9]:
-
-
 sns.countplot(df['Potability'])
 plt.show()
-
-
-# In[10]:
-
-
 sns.distplot(df['ph'])
 plt.show()
-
-
-# In[11]:
-
-
 df.hist(figsize=(14,14))
 plt.show()
-
-
-# In[12]:
-
-
 plt.figure(figsize=(13,8))
 sns.heatmap(df.corr(),annot=True,cmap='terrain')
 plt.show()
-
-
-# In[13]:
-
-
 df.boxplot(figsize=(14,7))
-
-
-# In[14]:
-
-
 X = df.drop('Potability',axis=1)
 Y= df['Potability']
-
-
-# In[15]:
-
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size= 0.2, random_state=101,shuffle=True)
-
-
-# In[16]:
-
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 dt=DecisionTreeClassifier(criterion= 'gini', min_samples_split= 10, splitter= 'best')
 dt.fit(X_train,Y_train)
-
-
-# In[17]:
-
-
 prediction=dt.predict(X_test)
 print(f"Accuracy Score = {accuracy_score(Y_test,prediction)*100}")
 print(f"Confusion Matrix =\n {confusion_matrix(Y_test,prediction)}")
 print(f"Classification Report =\n {classification_report(Y_test,prediction)}")
-
-
-# In[18]:
-
-
 res = dt.predict([[5.735724, 158.318741,25363.016594,7.728601,377.543291,568.304671,13.626624,75.952337,4.732954]])[0]
 res
-
-
-# In[19]:
-
-
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import GridSearchCV
-
-# define models and parameters
 model = DecisionTreeClassifier()
 criterion = ["gini", "entropy"]
 splitter = ["best", "random"]
 min_samples_split = [2,4,6,8,10,12,14]
-
-# define grid search
 grid = dict(splitter=splitter, criterion=criterion, min_samples_split=min_samples_split)
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 grid_search_dt = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, 
                            scoring='accuracy',error_score=0)
 grid_search_dt.fit(X_train, Y_train)
-
-
-# In[20]:
-
-
 print(f"Best: {grid_search_dt.best_score_:.3f} using {grid_search_dt.best_params_}")
 means = grid_search_dt.cv_results_['mean_test_score']
 stds = grid_search_dt.cv_results_['std_test_score']
